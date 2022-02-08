7bd73817776f8462ebfad58feecf1a818876daa9
deniz <yilmaz.deniz.itu@gmail.com>
2022-02-08 23:57:32 +0300

    clean the old form

--- src/components/Authors.js
+++ src/components/Authors.js
@@ -59,23 +59,9 @@ const Authors = (props) => {
         </tbody>
       </table>
       <h2>Set Birthyear</h2>
-
       <form onSubmit={submit}>
-        {/* <div>
-          name <input
-            value={author}
-            onChange={({ target }) => setAuthor(target.value)}
-          />
-        </div>
         <div>
-          born <input
-            value={born}
-            onChange={({ target }) => setBorn(Number(target.value))}
-          />
-        </div>
-        <button type='submit'>update author</button> */}
-        <div>
-          name <select value={author} onChange={({ target }) => setAuthor(target.value)} onSelect={({ target }) => setAuthor(target.value)}>
+          name <select value={author} onChange={({ target }) => setAuthor(target.value)}>
             <option ></option>
             {
               authors.map(a => 
